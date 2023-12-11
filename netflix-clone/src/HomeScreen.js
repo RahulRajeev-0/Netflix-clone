ae505ed651176917141fb844dc38040ba5d60431
Rahul Rajeev <rahulrajeev836@gmail.com>
2023-12-11 14:48:06 +0530

    navbar add

---
+++ netflix-clone/src/HomeScreen.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./HomeScreen.css";
+import Nav from './Nav';
+
+function HomeScreen() {
+  return (
+    <div>
+       <Nav/>
+        
+        {/* banner */}
+
+        {/* rows  */}
+
+      
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCmlDjmfZT6YLUpiVIcV-2H6wmz7bZUBFM",
+    authDomain: "netflix-clone-b556b.firebaseapp.com",
+    projectId: "netflix-clone-b556b",
+    storageBucket: "netflix-clone-b556b.appspot.com",
+    messagingSenderId: "1090180666784",
+    appId: "1:1090180666784:web:136f932299d452017140c5"
+  };
